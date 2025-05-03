d48df3f6ce39f58ed485806173ea1627d1576c4a
Ace <ooxxqaws@gmail.com>
2025-05-04 01:10:27 +0800

    [feat] 更新 get_lumos 方法提供技術學習資源連結

--- app/utils/lumos.py
+++ app/utils/lumos.py
@@ -4,4 +4,20 @@ logger = logging.getLogger(__name__)
 
 
 def get_lumos():
-    return "我們正在努力開發此功能，敬請期待"
+    return """ ✨ 技術學習資源 ✨
+    📌 Git：https://pse.is/7j2gcd
+    📌 IntelliJ：https://pse.is/7j2gem
+    📌 Java：https://pse.is/7j2gk2
+    📌 Spring Boot (1)：https://pse.is/7j2jtm
+    📌 Spring Boot (2)：https://pse.is/7j2gu8
+    📌 Java Message Service：https://pse.is/7j2gxm
+    📌 ActiveMQ：https://pse.is/7j2jaf
+    📌 Eureka Service：https://pse.is/7j2gyt
+    📌 Config Service：https://pse.is/7j2gzd
+    📌 Gateway Service：https://pse.is/7j2gzs
+    📌 Docker：https://pse.is/7j2gvc
+    📌 Nginx：https://pse.is/7j2gw5
+    📌 DataBase：https://pse.is/7j2gwq
+    📌 LINE Bot：https://pse.is/7j2h2j
+    📌 Oracle VirtualBox：https://pse.is/7j2j9a
+    """
