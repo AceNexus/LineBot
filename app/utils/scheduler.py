8dd5933c2f305ea982890a3b7d02181789061674
Ace <ooxxqaws@gmail.com>
2025-06-16 16:52:21 +0800

    [refactor] 重構排程器以支援多語言訂閱

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -14,7 +14,7 @@ def init_scheduler():
     scheduler = BackgroundScheduler()
 
     # 設定英文訂閱排程
-    setup_language_schedule(scheduler, ENGLISH_TIMES, 'english')
+    _setup_subscription_schedule(scheduler, ENGLISH_TIMES, 'english')
 
     # TODO 設定日文訂閱排程
 
@@ -23,7 +23,7 @@ def init_scheduler():
     return scheduler
 
 
-def setup_language_schedule(scheduler, subscription_times, language):
+def _setup_subscription_schedule(scheduler, subscription_times, language):
     """設定特定語言的訂閱排程"""
     for time_id, time_str in subscription_times.items():
         hour, minute = map(int, time_str.split(':'))
@@ -39,24 +39,24 @@ def setup_language_schedule(scheduler, subscription_times, language):
 
 
 def send_subscription_notification(time_id, language):
-    """發送訂閱通知的函數"""
+    """發送訂閱通知"""
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logger.info(f"Executing {language} {time_id} subscription notification - {current_time}")
 
     # 根據語言分發不同的通知邏輯
     if language == 'english':
-        send_english_notification()
+        send_english_notification(time_id)
     elif language == 'japanese':
-        send_japanese_notification()
+        send_japanese_notification(time_id)
     else:
         logger.warning(f"Unknown language type: {language}")
 
 
-def send_english_notification():
+def send_english_notification(time_id):
     """發送英文訂閱通知"""
-    print(f"English subscription sent")
+    print(f"English subscription sent for {time_id}")
 
 
-def send_japanese_notification():
+def send_japanese_notification(time_id):
     """發送日文訂閱通知"""
-    print(f"Japanese subscription sent")
+    print(f"Japanese subscription sent for {time_id}")
